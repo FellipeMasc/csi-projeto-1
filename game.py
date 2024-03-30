1ade5ffffb23e626344fe8c0d006bcb96da40eb0
laugusto07 <luis.augusto.tete@gmail.com>
2024-03-30 00:52:36 -0300

    fds

--- game.py
+++ game.py
@@ -1,35 +1,42 @@
 import pygame
+
 import sys
 
-from scripts.utils import load_image
+from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity
+from scripts.tilemap import Tilemap
 
 class Game:
     def __init__(self):
         pygame.init()
 
         pygame.display.set_caption("ITA Fight Club")
-
-        self.screen = pygame.display.set_mode((1280, 720))
-
-        self.display = pygame.Surface((320,240))
+        self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
 
         self.clock = pygame.time.Clock()
 
         self.movement = [False, False]
 
         self.assets = {
-            'player' : load_image('entities/player.png')
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+            'player': load_image('entities/player.png')
         }
 
-        self.player = PhysicsEntity(self, 'player', (50,50), (8, 15))
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
 
+        self.tilemap = Tilemap(self, tile_size=16)
 
     def run(self):
         while True:
             self.display.fill((14,219,248))
 
-            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.tilemap.render(self.display)
+
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
 
             for event in pygame.event.get():
@@ -41,6 +48,8 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
@@ -51,7 +60,4 @@ class Game:
             pygame.display.update()  # Refresh on-screen display
             self.clock.tick(60)  # wai until next frame (at 60 FPS)
 
-
 Game().run()
-
-
