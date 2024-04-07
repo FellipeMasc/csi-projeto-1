23ee5b5dcca15603585cde4c82b7d6a5bbd1479d
Fellipe <fellipe_sdp@hotmail.com>
2024-04-07 11:14:33 -0300

    [feat] finished stamina && especial attack

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ from scripts.utils import load_image, load_images, Animation
 from scripts.entities import PhysicsEntity, Player
 from scripts.tilemap import Tilemap
 
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -11,70 +12,91 @@ class Game:
         pygame.display.set_caption("ITA Fight Club")
         self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((320, 240))
-        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
-        self.img.set_colorkey((0,0,0))
+        self.img = pygame.image.load("data/images/clouds/cloud_1.png")
+        self.img.set_colorkey((0, 0, 0))
         self.clock = pygame.time.Clock()
 
         self.movement_p1 = [False, False]
         self.movement_p2 = [False, False]
-        #punch, kick, special, cokespecial
-        self.attack_p1 = [False,False,False,False]
-        self.attack_p2 = [False,False,False,False]
+        # punch, kick, special, cokespecial
+        self.attack_p1 = [False, False, False, False]
+        self.attack_p2 = [False, False, False, False]
         self.block_p1 = False
         self.block_p2 = False
         self.assets = {
-            'decor': load_images('tiles/decor'),
-            'grass': load_images('tiles/grass'),
-            'large_decor': load_images('tiles/large_decor'),
-            'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png'),
-            'background': load_image('background.png'),
-            'player1/idle': Animation(load_images('entities/player/idle'), img_dur=6),
-            'player1/run': Animation(load_images('entities/player/run'), img_dur=4),
-            'player1/jump': Animation(load_images('entities/player/jump')),
-            'player1/slide': Animation(load_images('entities/player/slide')),
-            'player1/wall_slide': Animation(load_images('entities/player/wall_slide')),
-            'player1/punch': Animation(load_images('entities/player/slide')),
-            'player1/jump_attack': Animation(load_images('entities/player/slide')),
-            'player1/kick': Animation(load_images('entities/player/wall_slide')),
-            'player1/block': Animation(load_images('entities/player/wall_slide')),
-            'player2/idle': Animation(load_images('entities/player/idle'), img_dur=6),
-            'player2/run': Animation(load_images('entities/player/run'), img_dur=4),
-            'player2/jump': Animation(load_images('entities/player/jump')),
-            'player2/slide': Animation(load_images('entities/player/slide')),
-            'player2/wall_slide': Animation(load_images('entities/player/wall_slide')),
-            'player2/punch': Animation(load_images('entities/player/slide')),
-            'player2/jump_attack': Animation(load_images('entities/player/slide')),
-            'player2/kick': Animation(load_images('entities/player/wall_slide')),
-            'player2/block': Animation(load_images('entities/player/wall_slide'))
+            "decor": load_images("tiles/decor"),
+            "grass": load_images("tiles/grass"),
+            "large_decor": load_images("tiles/large_decor"),
+            "stone": load_images("tiles/stone"),
+            "player": load_image("entities/player.png"),
+            "background": load_image("background.png"),
+            "particle/particle": Animation(load_images("particles/particle"), img_dur=6, loop=False),
+            "player1/idle": Animation(load_images("entities/player/idle"), img_dur=6),
+            "player1/run": Animation(load_images("entities/player/run"), img_dur=4),
+            "player1/jump": Animation(load_images("entities/player/jump")),
+            "player1/slide": Animation(load_images("entities/player/slide")),
+            "player1/wall_slide": Animation(load_images("entities/player/wall_slide")),
+            "player1/punch": Animation(load_images("entities/player/slide")),
+            "player1/jump_attack": Animation(load_images("entities/player/slide")),
+            "player1/kick": Animation(load_images("entities/player/wall_slide")),
+            "player1/block": Animation(load_images("entities/player/wall_slide")),
+            "player2/idle": Animation(load_images("entities/player/idle"), img_dur=6),
+            "player2/run": Animation(load_images("entities/player/run"), img_dur=4),
+            "player2/jump": Animation(load_images("entities/player/jump")),
+            "player2/slide": Animation(load_images("entities/player/slide")),
+            "player2/wall_slide": Animation(load_images("entities/player/wall_slide")),
+            "player2/punch": Animation(load_images("entities/player/slide")),
+            "player2/jump_attack": Animation(load_images("entities/player/slide")),
+            "player2/kick": Animation(load_images("entities/player/wall_slide")),
+            "player2/block": Animation(load_images("entities/player/wall_slide")),
         }
 
         self.player1 = Player(self, (50, 50), (8, 15), 1)
         self.player2 = Player(self, (100, 50), (8, 15), 2)
 
-        
         self.tilemap = Tilemap(self, tile_size=16)
+        self.particles = []
 
         self.scroll_1 = [0, 0]
         self.scroll_2 = [0, 0]
-        
 
     def run(self):
         while True:
-            self.display.blit(self.assets['background'], (0, 0))
+            self.display.blit(self.assets["background"], (0, 0))
 
             self.scroll_1[0] += (self.player1.rect().centerx - self.display.get_width() / 2 - self.scroll_1[0]) / 30
             self.scroll_1[1] += (self.player1.rect().centery - self.display.get_height() / 2 - self.scroll_1[1]) / 30
             self.scroll_2[0] += (self.player2.rect().centerx - self.display.get_width() / 2 - self.scroll_2[0]) / 30
             self.scroll_2[1] += (self.player2.rect().centery - self.display.get_height() / 2 - self.scroll_2[1]) / 30
-            render_scroll = (int(min(self.scroll_1[0], self.scroll_2[0])), int(min(self.scroll_1[1], self.scroll_2[1])))
+            render_scroll = (
+                int(min(self.scroll_1[0], self.scroll_2[0])),
+                int(min(self.scroll_1[1], self.scroll_2[1])),
+            )
             self.tilemap.render(self.display, offset=render_scroll)
 
             self.player1.render(self.display, offset=render_scroll)
-            self.player1.update(self.tilemap, self.player2, self.attack_p1, (self.movement_p1[1] - self.movement_p1[0], 0), self.block_p1)
+            self.player1.update(
+                self.tilemap,
+                self.player2,
+                self.attack_p1,
+                (self.movement_p1[1] - self.movement_p1[0], 0),
+                self.block_p1,
+            )
 
             self.player2.render(self.display, offset=render_scroll)
-            self.player2.update(self.tilemap,  self.player1,self.attack_p2, (self.movement_p2[1] - self.movement_p2[0], 0), self.block_p2)
+            self.player2.update(
+                self.tilemap,
+                self.player1,
+                self.attack_p2,
+                (self.movement_p2[1] - self.movement_p2[0], 0),
+                self.block_p2,
+            )
+            
+            for particle in self.particles.copy():
+                kill = particle.update()
+                particle.render(self.display, offset = render_scroll)
+                if kill:
+                    self.particles.remove(particle)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -90,7 +112,7 @@ class Game:
                     if event.key == pygame.K_k:
                         self.attack_p1[1] = True
                     if event.key == pygame.K_l:
-                        self.attack_p1[2] = True
+                        self.player1.especial_attack(self)
                     if event.key == pygame.K_a:
                         self.movement_p2[0] = True
                     if event.key == pygame.K_d:
@@ -100,7 +122,7 @@ class Game:
                     if event.key == pygame.K_x:
                         self.attack_p2[1] = True
                     if event.key == pygame.K_c:
-                        self.attack_p2[2] = True
+                        self.player2.especial_attack(self)
                     if event.key == pygame.K_UP:
                         self.player1.jump()
                     if event.key == pygame.K_w:
@@ -109,6 +131,10 @@ class Game:
                         self.block_p1 = True
                     if event.key == pygame.K_v:
                         self.block_p2 = True
+                    if event.key == pygame.K_f:
+                        self.player1.dash()
+                    if event.key == pygame.K_m:
+                        self.player2.dash()
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement_p1[0] = False
@@ -122,21 +148,21 @@ class Game:
                         self.attack_p1[0] = False
                     if event.key == pygame.K_k:
                         self.attack_p1[1] = False
-                    if event.key == pygame.K_l:
-                        self.attack_p1[2] = False
                     if event.key == pygame.K_z:
                         self.attack_p2[0] = False
                     if event.key == pygame.K_x:
                         self.attack_p2[1] = False
-                    if event.key == pygame.K_c:
-                        self.attack_p2[2] = False
                     if event.key == pygame.K_n:
                         self.block_p1 = False
                     if event.key == pygame.K_v:
                         self.block_p2 = False
 
-            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
-            pygame.display.update() 
-            self.clock.tick(60) 
+            self.screen.blit(
+                pygame.transform.scale(self.display, self.screen.get_size()),
+                (0, 0),
+            )
+            pygame.display.update()
+            self.clock.tick(60)
+
 
 Game().run()
--- scripts/entities.py
+++ scripts/entities.py
@@ -1,41 +1,58 @@
+import sys
 import pygame
+from .utils import HealthBar, EspecialBar, Particles
+
+
 class PhysicsEntity:
     def __init__(self, game, e_type, pos, size):
         self.game = game
         self.e_type = e_type
         self.pos = list(pos)
         self.size = size
-        self.velocity = [0,0]
-        self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
+        self.velocity = [0, 0]
+        self.collisions = {
+            "up": False,
+            "down": False,
+            "right": False,
+            "left": False,
+        }
 
-        self.action = ''
+        self.action = ""
         self.anim_offset = (-3, -3)
         self.flip = False
-        self.set_action('idle')
+        self.set_action("idle")
 
     def rect(self):
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
-    
+
     def set_action(self, action):
         if action != self.action:
             self.action = action
-            self.animation = self.game.assets[self.e_type + '/' + self.action].copy()
+            self.animation = self.game.assets[self.e_type + "/" + self.action].copy()
 
-    def update(self, tilemap, movement=(0,0)):
-        self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
+    def update(self, tilemap, movement=(0, 0)):
+        self.collisions = {
+            "up": False,
+            "down": False,
+            "right": False,
+            "left": False,
+        }
 
-        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+        frame_movement = (
+            movement[0] + self.velocity[0],
+            movement[1] + self.velocity[1],
+        )
 
         self.pos[0] += frame_movement[0]
         entity_rect = self.rect()
         for rect in tilemap.physics_rects_around(self.pos):
             if entity_rect.colliderect(rect):
                 if frame_movement[0] > 0:
-                   entity_rect.right = rect.left
-                   self.collisions['right'] = True
+                    entity_rect.right = rect.left
+                    self.collisions["right"] = True
                 if frame_movement[0] < 0:
-                   entity_rect.left = rect.right 
-                   self.collisions['left'] = True
+                    entity_rect.left = rect.right
+                    self.collisions["left"] = True
                 self.pos[0] = entity_rect.x
 
         self.pos[1] += frame_movement[1]
@@ -43,11 +60,11 @@ class PhysicsEntity:
         for rect in tilemap.physics_rects_around(self.pos):
             if entity_rect.colliderect(rect):
                 if frame_movement[1] > 0:
-                   entity_rect.bottom = rect.top
-                   self.collisions['down'] = True
+                    entity_rect.bottom = rect.top
+                    self.collisions["down"] = True
                 if frame_movement[1] < 0:
-                   entity_rect.top = rect.bottom
-                   self.collisions['up'] = True 
+                    entity_rect.top = rect.bottom
+                    self.collisions["up"] = True
                 self.pos[1] = entity_rect.y
 
         if movement[0] > 0:
@@ -55,124 +72,149 @@ class PhysicsEntity:
         if movement[0] < 0:
             self.flip = True
 
-        self.velocity[1] = min(5, self.velocity[1]+ 0.1)
+        self.velocity[1] = min(5, self.velocity[1] + 0.1)
 
-        if self.collisions['down'] or self.collisions['up']:
+        if self.collisions["down"] or self.collisions["up"]:
             self.velocity[1] = 0
 
         self.animation.update()
 
-    def render(self, surf, offset=(0,0)):
-        surf.blit(pygame.transform.flip(self.animation.img(), self.flip, False), (self.pos[0] - offset[0] + self.anim_offset[0], self.pos[1] - offset[1] + self.anim_offset[1]))
+    def render(self, surf, offset=(0, 0)):
+        surf.blit(
+            pygame.transform.flip(self.animation.img(), self.flip, False),
+            (
+                self.pos[0] - offset[0] + self.anim_offset[0],
+                self.pos[1] - offset[1] + self.anim_offset[1],
+            ),
+        )
+
 
 class Player(PhysicsEntity):
     def __init__(self, game, pos, size, player_number):
-        super().__init__(game, 'player{}'.format(player_number), pos, size)
+        super().__init__(game, "player{}".format(player_number), pos, size)
         self.air_time = 0
         self.jumps = 1
         self.health = 100
+        self.stamina = 0
         self.melee_attack = False
         self.block = False
+        self.dashing = 0
+        self.player_number = player_number
         self.heath_bar = HealthBar(game, self, player_number)
-    def update(self, tilemap, other_player,attack=[False,False,False,False], movement=(0,0), block = False):
+        self.especial_bar = EspecialBar(game,self,player_number)
+
+    def update(
+        self,
+        tilemap,
+        other_player,
+        attack=[False, False, False, False],
+        movement=(0, 0),
+        block=False,
+    ):
         super().update(tilemap, movement=movement)
         offset_bottom = 6
-        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+        frame_movement = (
+            movement[0] + self.velocity[0],
+            movement[1] + self.velocity[1],
+        )
         entity_rect = self.rect()
         other_rect = other_player.rect()
-        if(abs(entity_rect.left - other_rect.right) < 3 or abs(entity_rect.right - other_rect.left) < 3):
-            print(entity_rect.top,other_rect.top)
-            if(other_player.melee_attack and abs(entity_rect.top - other_rect.top) < 8 and not self.block):
+        if abs(entity_rect.left - other_rect.right) < 3 or abs(entity_rect.right - other_rect.left) < 3:
+            if other_player.melee_attack and abs(entity_rect.top - other_rect.top) < 8 and not self.block:
                 self.health = self.health - 0.1
+                other_player.stamina = min(100,other_player.stamina + 1)
                 self.heath_bar.update()
-                
-        
-        
-        if(entity_rect.colliderect(other_rect)):
+                other_player.especial_bar.update()
+
+        if entity_rect.colliderect(other_rect):
             if frame_movement[0] > 0 and entity_rect.bottom > other_rect.top + offset_bottom:
                 entity_rect.right = other_rect.left
-                self.collisions['right'] = True
+                self.collisions["right"] = True
                 self.pos[0] = entity_rect.x
             if frame_movement[0] < 0 and entity_rect.bottom > other_rect.top + offset_bottom:
-                entity_rect.left = other_rect.right 
-                self.collisions['left'] = True
+                entity_rect.left = other_rect.right
+                self.collisions["left"] = True
                 self.pos[0] = entity_rect.x
             if frame_movement[1] > 0 and entity_rect.top < other_rect.top:
                 entity_rect.bottom = other_rect.top
-                self.collisions['down'] = True
+                self.collisions["down"] = True
                 self.pos[1] = entity_rect.y
             if frame_movement[1] < 0:
-                self.collisions['up'] = True 
+                self.collisions["up"] = True
 
-
-        
-        
         self.air_time += 1
-        if self.collisions['down']:
+        if self.collisions["down"]:
             self.air_time = 0
             self.jumps = 1
-            
 
         self.wall_slide = False
-        if (self.collisions['right'] or self.collisions['left']) and self.air_time > 4:
-           self.wall_slide = True 
-           self.velocity[1] = min(self.velocity[1], 0.5)
-        
+        if (self.collisions["right"] or self.collisions["left"]) and self.air_time > 4:
+            self.wall_slide = True
+            self.velocity[1] = min(self.velocity[1], 0.5)
+
         if self.air_time > 4:
-            if(attack[0] or attack[1]):
-                self.set_action('jump_attack')
+            if attack[0] or attack[1]:
+                self.set_action("jump_attack")
             else:
-                self.set_action('jump')
+                self.set_action("jump")
         elif movement[0] != 0:
-            self.set_action('run')
+            self.set_action("run")
         elif attack[0]:
-            self.set_action('punch')
+            self.set_action("punch")
         elif attack[1]:
-            self.set_action('kick')
+            self.set_action("kick")
         elif block:
-            self.set_action('block')
+            self.set_action("block")
         else:
-            self.set_action('idle')
+            self.set_action("idle")
+
+        if self.dashing > 0:
+            self.dashing = max(0, self.dashing - 1)
+        if self.dashing < 0:
+            self.dashing = min(0, self.dashing + 1)
+        if abs(self.dashing) > 50:
+            self.velocity[0] = abs(self.dashing) / self.dashing * 8
+            if abs(self.dashing) == 51:
+                self.velocity[0] *= 0.1
+
+        if self.velocity[0] > 0:
+            self.velocity[0] = max(self.velocity[0] - 0.1, 0)
+        else:
+            self.velocity[0] = min(self.velocity[0] + 0.1, 0)
             
+        for particle in self.game.particles:
+            if self.rect().collidepoint(particle.pos) and particle.player_number != self.player_number:
+                self.health = self.health - 10
+                self.heath_bar.update()
+        
+
     def set_action(self, action):
         self.melee_attack = ["punch", "kick", "jump_attack"].count(action) > 0
         self.block = action == "block"
-        return super().set_action(action)        
-            
-            
+        return super().set_action(action)
+
     def jump(self):
-      if self.jumps:
-              self.velocity[1] = -3
-              self.jumps -= 1
-              self.air_time = 5
-              
+        if self.jumps:
+            self.velocity[1] = -3
+            self.jumps -= 1
+            self.air_time = 5
+
     def dash(self):
         if not self.dashing:
             if self.flip:
                 self.dashing = -60
             else:
-                self.dashing = 60          
-    
+                self.dashing = 60
+                
+                
+    def especial_attack(self, game):
+        if(self.stamina == 100):
+            velocityx = -5 if self.flip else 5
+            game.particles.append(Particles(game,'particle', [self.rect().centerx, self.rect().centery],self.player_number, [velocityx, 0]))
+            self.stamina = 0
+            self.especial_bar.update()
+
     def render(self, surf, offset=(0, 0)):
         self.heath_bar.render(surf)
+        self.especial_bar.render(surf)
         return super().render(surf, offset)
-    
-    
-              
-class HealthBar:
-    def __init__(self, game, player, player_number):
-        self.game = game
-        self.player = player
-        self.player_number = player_number
-        self.width = 100
-        self.height = 10
-        self.border_width = 1
-        self.x = 20 if player_number == 1 else 200
-        self.y = 20 
-
-    def update(self):
-        self.width = int((self.player.health / 100) * 100)
-
-    def render(self, surf):
-        pygame.draw.rect(surf, (255, 0, 0), (self.x, self.y, self.width, self.height))  
-
